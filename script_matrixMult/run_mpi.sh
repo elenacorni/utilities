a678ebdccef1b84cf2c68f4315c29dea5f574e66
Elena <elena.corni@libero.it>
2016-11-22 17:36:54 +0100

    Update run_mpi.sh

--- script_matrixMult/run_mpi.sh
+++ script_matrixMult/run_mpi.sh
@@ -9,6 +9,10 @@ do
                         if [ ${warmup} -eq 0 -a ${proc} -eq 2 ]; then
                                 echo mpirun -np ${proc} --allow-run-as-root ./mtxMul.mpi.O3 -rA=${sizeMtx} -cA=${sizeMtx} -cB=${sizeMtx} -w=${warmup}
                         fi
+                        # [[20356,1],0]: A high-performance Open MPI point-to-point messaging
+                        # module was unable to find any relevant network interfaces:
+                        # Module: OpenFabrics (openib)
+                        # Aggiungere a mpirun: --mca btl ^openib (es. mpirun -np 16 --mca btl ^openib ./a.out)
                         mpirun -np ${proc} --allow-run-as-root ./mtxMul.mpi.O3 -rA=${sizeMtx} -cA=${sizeMtx} -cB=${sizeMtx} -w=${warmup}
                 done
                 echo " "
