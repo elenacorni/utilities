5ff3378e8a05b061e1ee47c4e65c4214ddc9d173
Elena <elena.corni@libero.it>
2016-11-30 11:25:19 +0100

    Create setup_intel_compiler.sh

---
+++ CMSSW/setup_intel_compiler.sh
@@ -0,0 +1,3 @@
+scram setup /data/fwyzard/IntelWorkshop/tools/icc-ccompiler.xml
+scram setup /data/fwyzard/IntelWorkshop/tools/icc-cxxcompiler.xml
+scram setup /data/fwyzard/IntelWorkshop/tools/icc-f77compiler.xml cmsenv
