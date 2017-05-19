b8140dbc4dc1017416c4cb5ab983a7098706098a
Elena <elena.corni@libero.it>
2017-05-19 15:05:35 +0200

    Update cosa-test-mic.sh

--- vari/cosa-test-mic.sh
+++ vari/cosa-test-mic.sh
@@ -1,10 +1,8 @@
-# per caricare il compilatore gcc-6.2.1:
+# compilatore gcc-6.2.1:
 scl enable devtoolset-6 bash
 
-
-# per compilare le OpenBlas:
+# compilare le OpenBlas:
 make (clean) TARGET=NEHALEM
 
-
-# per caricare il compilatore intel:
+# compilatore intel:
 source /opt/intel/compilers_and_libraries/linux/bin/compilervars.sh -arch intel64 -platform linux
